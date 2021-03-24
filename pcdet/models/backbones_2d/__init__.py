cb038c0bd304a4a1c22de0fd17f7cd4fd67c2944
triasamo1 <triasamo1@ee.duth.gr>
2021-03-24 14:19:18 +0200

    Add my Backbone_2d - Evaluation works so far

--- pcdet/models/backbones_2d/__init__.py
+++ pcdet/models/backbones_2d/__init__.py
@@ -1,5 +1,8 @@
 from .base_bev_backbone import BaseBEVBackbone
+from .base_bev_backbone_trias import BaseBEVBackbone_trias
+
 
 __all__ = {
-    'BaseBEVBackbone': BaseBEVBackbone
+    'BaseBEVBackbone': BaseBEVBackbone,
+    'BaseBEVBackbone_trias': BaseBEVBackbone_trias
 }
