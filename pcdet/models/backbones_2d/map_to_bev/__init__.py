432070ea716509b011d832a2c7221329b4a68742
triasamo1 <triasamo1@ee.duth.gr>
2021-03-24 14:11:14 +0200

    Add my MAP_TO_BEV - Evaluation works so far

--- pcdet/models/backbones_2d/map_to_bev/__init__.py
+++ pcdet/models/backbones_2d/map_to_bev/__init__.py
@@ -1,7 +1,9 @@
 from .height_compression import HeightCompression
 from .pointpillar_scatter import PointPillarScatter
+from .pointpillar_scatter_trias import PointPillarScatter_trias
 
 __all__ = {
     'HeightCompression': HeightCompression,
-    'PointPillarScatter': PointPillarScatter
+    'PointPillarScatter': PointPillarScatter,
+    'PointPillarScatter_trias': PointPillarScatter_trias
 }
