b602b6323d0d76c26173d6412621204233f55c27
triasamo1 <triasamo1@ee.duth.gr>
2021-03-24 12:39:39 +0200

    Add my VFE - Evaluation works so far

--- pcdet/models/backbones_3d/vfe/__init__.py
+++ pcdet/models/backbones_3d/vfe/__init__.py
@@ -1,9 +1,11 @@
 from .mean_vfe import MeanVFE
 from .pillar_vfe import PillarVFE
+from .pillar_vfe_trias import PillarVFE_trias
 from .vfe_template import VFETemplate
 
 __all__ = {
     'VFETemplate': VFETemplate,
     'MeanVFE': MeanVFE,
-    'PillarVFE': PillarVFE
+    'PillarVFE': PillarVFE,
+    'PillarVFE_trias': PillarVFE_trias
 }
