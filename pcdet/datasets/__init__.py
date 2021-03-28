6917c9b6fd6584a78dca74a14dd75501f69e209e
triasamo1 <triasamo1@ee.duth.gr>
2021-03-28 15:27:33 +0300

    Edit code to carry 3D bbox coordinates for evaluation purposes

--- pcdet/datasets/__init__.py
+++ pcdet/datasets/__init__.py
@@ -6,12 +6,14 @@ from pcdet.utils import common_utils
 
 from .dataset import DatasetTemplate
 from .kitti.kitti_dataset import KittiDataset
+from .kitti.kitti_dataset_trias import KittiDataset_trias
 from .nuscenes.nuscenes_dataset import NuScenesDataset
 from .waymo.waymo_dataset import WaymoDataset
 
 __all__ = {
     'DatasetTemplate': DatasetTemplate,
     'KittiDataset': KittiDataset,
+    'KittiDataset_trias': KittiDataset_trias,
     'NuScenesDataset': NuScenesDataset,
     'WaymoDataset': WaymoDataset
 }
